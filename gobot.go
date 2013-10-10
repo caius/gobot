3903e9144042c07ba58f409fc84eef1e9ab84d2d
Caius Durling <dev@caius.name>
2013-10-10 22:42:06 +0100

    Add the first plugin, <title> from URLs

--- gobot.go
+++ gobot.go
@@ -4,7 +4,58 @@ import (
   "log"
   "fmt"
   "github.com/thoj/go-ircevent"
+  "strings"
+  "regexp"
+  "net/http"
+  "io/ioutil"
 )
+
+
+// Listens to channel conversation and inserts title of any link posted, following redirects
+// `And then I went to www.caius.name` => `gobot: Caius Durling &raquo; Profile`
+func URLHandler(con *irc.Connection, e irc.Event, replyName string) {
+  msg := e.Message
+
+  fmt.Printf("URLHandler checking '%s'\n", msg)
+
+  // Regexp from http://daringfireball.net/2010/07/improved_regex_for_matching_urls - Ta gruber!
+  url_regexp := regexp.MustCompile("(?i)\\b((?:https?://|www\\d{0,3}[.]|[a-z0-9.\\-]+[.][a-z]{2,4}/)(?:[^\\s()<>]+|\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\))+(?:\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\)|[^\\s`!()\\[\\]{};:'\".,<>?«»“”‘’]))")
+  url := url_regexp.FindString(msg)
+
+  if url == "" {
+    return
+  }
+
+  fmt.Printf("Extracted '%s'\n", url)
+
+  // We might extract `www.google.com` or `bit.ly/something` so we need to prepend http:// in that case
+  if !strings.HasPrefix(url, "http://") {
+    url = fmt.Sprintf("http://%s", url)
+  }
+
+  fmt.Printf("GET %s\n", url)
+
+  // Attempt a GET request to get the page title
+  // TODO: handle PDF and non-HTML content
+  resp, err := http.Get(url)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  defer resp.Body.Close()
+  raw_body, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  body := string(raw_body)
+
+  title_regexp := regexp.MustCompile("<title>([^<]+)</title>")
+  title := title_regexp.FindStringSubmatch(body)[1]
+
+  fmt.Printf("title: %s\n", title)
+  con.Privmsg(replyName, title)
+}
 
 func main() {
   fmt.Printf("") // FU GO
@@ -23,9 +74,11 @@ func main() {
   })
 
   con.AddCallback("PRIVMSG", func(e *irc.Event) {
-    fmt.Printf("[%6s] %6s: %s\n", e.Nick, e.Message)
+    // TODO: have irc.Event contain the room name for the PRIVMSG
+    fmt.Printf("[%6s] %6s: %s\n", roomName, e.Nick, e.Message)
 
-    // "Plugins" go here
+    // "Plugins"
+    URLHandler(con, *e, roomName)
   })
 
   con.Loop()
