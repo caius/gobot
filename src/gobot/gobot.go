26ba984ccc069f54e4641b5b4661cd07e0f27c17
Caius Durling <dev@caius.name>
2013-11-06 20:13:15 +0000

    Add plugins as a map to iterate

--- src/gobot/gobot.go
+++ src/gobot/gobot.go
@@ -15,124 +15,128 @@ import (
 var GitCommit string
 var BuiltBy string
 
-func Version(con *irc.Connection, e irc.Event, replyName string) {
-  if regexp.MustCompile("^version").MatchString(e.Message) {
-    reply := "My current version is"
+var Plugins = map[string]func(con *irc.Connection, e irc.Event, replyName string){
+  "version": func(con *irc.Connection, e irc.Event, replyName string) {
+    if regexp.MustCompile("^version").MatchString(e.Message) {
+      reply := "My current version is"
 
-    if GitCommit != "" {
-       reply = fmt.Sprintf("%s %s", reply, GitCommit)
-    } else {
-      reply = fmt.Sprintf("%s unknown", reply)
+      if GitCommit != "" {
+         reply = fmt.Sprintf("%s %s", reply, GitCommit)
+      } else {
+        reply = fmt.Sprintf("%s unknown", reply)
+      }
+
+      if BuiltBy != "" {
+        reply = fmt.Sprintf("%s and I was built by %s", reply, BuiltBy)
+      }
+
+      con.Privmsgf(replyName, reply)
     }
+  },
 
-    if BuiltBy != "" {
-      reply = fmt.Sprintf("%s and I was built by %s", reply, BuiltBy)
+  "pong": func(con *irc.Connection, e irc.Event, replyName string) {
+    if regexp.MustCompile("^(?:\\.|!?\\.?ping)$").MatchString(e.Message) {
+      con.Privmsg(replyName, "pong!")
     }
+  },
+}
 
-    con.Privmsgf(replyName, reply)
+//*
+func Stats(con *irc.Connection, e irc.Event, replyName string) {
+  if regexp.MustCompile("^stats?$").MatchString(e.Message) {
+    con.Privmsg(replyName, "http://dev.hentan.caius.name/irc/nwrug.html")
   }
 }
 
-func Pong(con *irc.Connection, e irc.Event, replyName string) {
-	if regexp.MustCompile("^(?:\\.|!?\\.?ping)$").MatchString(e.Message) {
-		con.Privmsg(replyName, "pong!")
-	}
-}
-
-func Stats(con *irc.Connection, e irc.Event, replyName string) {
-	if regexp.MustCompile("^stats?$").MatchString(e.Message) {
-		con.Privmsg(replyName, "http://dev.hentan.caius.name/irc/nwrug.html")
-	}
-}
-
 func Dance(con *irc.Connection, e irc.Event, replyName string) {
-	if !regexp.MustCompile("^dance$").MatchString(e.Message) {
-		return
-	}
+  if !regexp.MustCompile("^dance$").MatchString(e.Message) {
+    return
+  }
 
-	i, err := rand.Int(rand.Reader, big.NewInt(2))
-	if err != nil {
-		// TODO: handle error
-		return // Oops!
-	}
+  i, err := rand.Int(rand.Reader, big.NewInt(2))
+  if err != nil {
+    // TODO: handle error
+    return // Oops!
+  }
 
-	switch i.Int64() {
-	case 0:
-		con.Privmsg(replyName, "EVERYBODY DANCE NOW!") // msg channel, "EVERYBODY DANCE NOW!"
-		// TODO: ACTION
-		con.Privmsg(replyName, "ACTION does the funky chicken")
-	case 1:
-		con.Privmsg(replyName, "http://no.gd/caiusboogie.gif")
-	case 2:
-		con.Privmsg(replyName, "http://i.imgur.com/rDDjz.gif")
-	}
+  switch i.Int64() {
+  case 0:
+    con.Privmsg(replyName, "EVERYBODY DANCE NOW!") // msg channel, "EVERYBODY DANCE NOW!"
+    // TODO: ACTION
+    con.Privmsg(replyName, "ACTION does the funky chicken")
+  case 1:
+    con.Privmsg(replyName, "http://no.gd/caiusboogie.gif")
+  case 2:
+    con.Privmsg(replyName, "http://i.imgur.com/rDDjz.gif")
+  }
 }
 
 // Stabs what he is comanded to. Unless it's himself.
 // `stab blah` => `* gobot stabs blah`
 func StabHandler(con *irc.Connection, e irc.Event, replyName string) {
-	msg := e.Message
+  msg := e.Message
 
-	stab_regexp := regexp.MustCompile("stab (.+)")
+  stab_regexp := regexp.MustCompile("stab (.+)")
 
-	if !stab_regexp.MatchString(msg) {
-		return
-	}
+  if !stab_regexp.MatchString(msg) {
+    return
+  }
 
-	receiver := stab_regexp.FindStringSubmatch(msg)[1]
-	// If they try to stab us, stab them
-	if regexp.MustCompile("rugbot").MatchString(receiver) {
-		receiver = e.Nick
-	}
+  receiver := stab_regexp.FindStringSubmatch(msg)[1]
+  // If they try to stab us, stab them
+  if regexp.MustCompile("rugbot").MatchString(receiver) {
+    receiver = e.Nick
+  }
 
-	con.Privmsgf(replyName, "/me stabs %s", receiver)
+  con.Privmsgf(replyName, "/me stabs %s", receiver)
 }
 
 // Listens to channel conversation and inserts title of any link posted, following redirects
 // `And then I went to www.caius.name` => `gobot: Caius Durling &raquo; Profile`
 func URLHandler(con *irc.Connection, e irc.Event, replyName string) {
-	msg := e.Message
+  msg := e.Message
 
-	fmt.Printf("URLHandler checking '%s'\n", msg)
+  fmt.Printf("URLHandler checking '%s'\n", msg)
 
-	// Regexp from http://daringfireball.net/2010/07/improved_regex_for_matching_urls - Ta gruber!
-	url_regexp := regexp.MustCompile("(?i)\\b((?:https?://|www\\d{0,3}[.]|[a-z0-9.\\-]+[.][a-z]{2,4}/)(?:[^\\s()<>]+|\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\))+(?:\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\)|[^\\s`!()\\[\\]{};:'\".,<>?«»“”‘’]))")
-	url := url_regexp.FindString(msg)
+  // Regexp from http://daringfireball.net/2010/07/improved_regex_for_matching_urls - Ta gruber!
+  url_regexp := regexp.MustCompile("(?i)\\b((?:https?://|www\\d{0,3}[.]|[a-z0-9.\\-]+[.][a-z]{2,4}/)(?:[^\\s()<>]+|\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\))+(?:\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\)|[^\\s`!()\\[\\]{};:'\".,<>?«»“”‘’]))")
+  url := url_regexp.FindString(msg)
 
-	if url == "" {
-		return
-	}
+  if url == "" {
+    return
+  }
 
-	fmt.Printf("Extracted '%s'\n", url)
+  fmt.Printf("Extracted '%s'\n", url)
 
-	// We might extract `www.google.com` or `bit.ly/something` so we need to prepend http:// in that case
-	if !strings.HasPrefix(url, "http://") {
-		url = fmt.Sprintf("http://%s", url)
-	}
+  // We might extract `www.google.com` or `bit.ly/something` so we need to prepend http:// in that case
+  if !strings.HasPrefix(url, "http://") {
+    url = fmt.Sprintf("http://%s", url)
+  }
 
-	fmt.Printf("GET %s\n", url)
+  fmt.Printf("GET %s\n", url)
 
-	// Attempt a GET request to get the page title
-	// TODO: handle PDF and non-HTML content
-	resp, err := http.Get(url)
-	if err != nil {
-		log.Fatal(err)
-	}
+  // Attempt a GET request to get the page title
+  // TODO: handle PDF and non-HTML content
+  resp, err := http.Get(url)
+  if err != nil {
+    log.Fatal(err)
+  }
 
-	defer resp.Body.Close()
-	raw_body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
+  defer resp.Body.Close()
+  raw_body, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    log.Fatal(err)
+  }
 
-	body := string(raw_body)
+  body := string(raw_body)
 
-	title_regexp := regexp.MustCompile("<title>([^<]+)</title>")
-	title := title_regexp.FindStringSubmatch(body)[1]
+  title_regexp := regexp.MustCompile("<title>([^<]+)</title>")
+  title := title_regexp.FindStringSubmatch(body)[1]
 
-	fmt.Printf("title: %s\n", title)
-	con.Privmsg(replyName, title)
+  fmt.Printf("title: %s\n", title)
+  con.Privmsg(replyName, title)
 }
+//*/
 
 func main() {
 	fmt.Printf("Version: %s\nBuilt by: %s\n", GitCommit, BuiltBy) // FU GO
@@ -155,12 +159,20 @@ func main() {
 		fmt.Printf("[%6s] %6s: %s\n", roomName, e.Nick, e.Message)
 
 		// "Plugins"
-		go StabHandler(con, *e, roomName)
-		go URLHandler(con, *e, roomName)
-		go Pong(con, *e, roomName)
-		go Stats(con, *e, roomName)
-		go Dance(con, *e, roomName)
-		go Version(con, *e, roomName)
+/*
+    go StabHandler(con, *e, roomName)
+    go URLHandler(con, *e, roomName)
+    go Pong(con, *e, roomName)
+    go Stats(con, *e, roomName)
+    go Dance(con, *e, roomName)
+    go Version(con, *e, roomName)
+//*/
+
+    for matchString := range Plugins {
+      // TODO: check matchString against e.Message
+      f := Plugins[matchString]
+      go f(con, *e, roomName)
+    }
 	})
 
 	con.Loop()
