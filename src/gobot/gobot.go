4dab786670d2ccd57611522208eea83685457dc4
Caius Durling <dev@caius.name>
2013-11-09 10:27:49 +0000

    Pull out bot logic into Gobot struct

--- src/gobot/gobot.go
+++ src/gobot/gobot.go
@@ -33,7 +33,78 @@ func (self *Privmsg) Action(response string) {
 	self.Msg(response)
 }
 
-func Sample(arr []string) (string, error) {
+type Gobot struct {
+	Name   string // Bot nick
+	Pass   string // Password for nickserv, if required (noop if empty)
+	Room   string // TODO: make an array once PRIVMSG handler can work out source of event
+	Server string // "server.name:port"
+
+	Plugins map[string]func(p Privmsg)
+
+	Con *irc.Connection
+}
+
+// Run the bot. Setup plugins before calling this, blocks execution until program end
+func (bot *Gobot) Run() {
+	bot.Con = irc.IRC(bot.Name, bot.Name) // Use the bot's nick as real name too
+	err := bot.Con.Connect(bot.Server)
+	if err != nil {
+		log.Fatal("Couldn't connect to %s: %s", bot.Server, err)
+	}
+
+	// Once we're successfully connected to the network
+	bot.Con.AddCallback("001", func(e *irc.Event) {
+		// Join our rooms!
+		bot.Con.Join(bot.Room)
+	})
+
+	// Handle messages from rooms we're in
+	bot.Con.AddCallback("PRIVMSG", func(e *irc.Event) {
+		// TODO: have irc.Event contain the room name for the PRIVMSG
+		fmt.Printf("[%6s] %6s: %s\n", bot.Name, e.Nick, e.Message)
+
+		privmsg := Privmsg{Connection: *bot.Con, Event: *e, Message: e.Message, Nick: e.Nick, RoomName: bot.Room}
+
+		// Plugins!
+		lowerMessage := strings.ToLower(privmsg.Message)
+
+		for matchString := range bot.Plugins {
+			// TODO: check matchString against e.Message
+			if strings.HasPrefix(matchString, "/") && strings.HasPrefix(matchString, "/") {
+				// Yer a regexp Harry
+				regexString := strings.TrimPrefix(matchString, "/")
+				regexString = strings.TrimSuffix(regexString, "/")
+
+				fmt.Printf("regexString: %s\n", regexString)
+
+				// Get on yer bike if we don't match
+				if !regexp.MustCompile(regexString).MatchString(e.Message) {
+					fmt.Printf("Skipping %s plugin - no regexp match\n", matchString)
+					continue
+				}
+			} else {
+				// Just a plain ole string
+				if !strings.Contains(lowerMessage, matchString) {
+					fmt.Printf("Skipping %s plugin - no string match\n", matchString)
+					continue
+				}
+			}
+
+			f := bot.Plugins[matchString]
+			f(privmsg)
+		}
+	})
+
+	bot.Con.Loop()
+}
+
+// Add handler for trigger
+func (bot *Gobot) Match(trigger string, handler func(privmsg Privmsg)) {
+	bot.Plugins[trigger] = handler
+}
+
+// Helper method for returning "random" responses
+func (bot *Gobot) Sample(arr []string) (string, error) {
 	max := int64(len(arr))
 	i, err := rand.Int(rand.Reader, big.NewInt(max))
 	if err != nil {
@@ -42,50 +113,58 @@ func Sample(arr []string) (string, error) {
 	return arr[i.Int64()], nil
 }
 
-var Plugins = map[string]func(privmsg Privmsg){
+func main() {
+	fmt.Printf("Version: %s\nBuilt by: %s\n", GitCommit, BuiltBy) // FU GO
 
-	"/help|commands/": func(privmsg Privmsg) {
+	bot := Gobot{Name: "gobot", Room: "#caius", Server: "irc.freenode.net:6667"}
+	bot.Plugins = make(map[string]func(p Privmsg))
+
+	bot.Match("hullo", func(privmsg Privmsg) {
+		privmsg.Msg("Oh hai!")
+	})
+
+	bot.Match("/help|commands/", func(privmsg Privmsg) {
 		privmsg.Msg("roll, nextmeet, artme <string>, stab <nick>, seen <nick>, ram, uptime, 37status, boobs, trollface, dywj, dance, mustachify, stats, last, ping")
-	},
+	})
 
-	"meme": func(privmsg Privmsg) {
+	bot.Match("meme", func(privmsg Privmsg) {
 		// There are no decent meme web services, nor gems wrapping the shitty ones.
 		// -- Caius, 20th Aug 2011
 		privmsg.Msg("Y U NO FIX MEME?!")
-	},
+	})
 
-	"/troll(face)?/": func(privmsg Privmsg) {
-		response, err := Sample([]string{"http://no.gd/troll.png", "http://no.gd/trolldance.gif", "http://caius.name/images/phone_troll.jpg"})
+	bot.Match("/troll(face)?/", func(privmsg Privmsg) {
+		response, err := bot.Sample([]string{"http://no.gd/troll.png", "http://no.gd/trolldance.gif", "http://caius.name/images/phone_troll.jpg"})
 		if err != nil {
 			return
 		}
 
 		privmsg.Msg(response)
-	},
+	})
 
-	"boner": func(privmsg Privmsg) {
-		response, err := Sample([]string{"http://files.myopera.com/coxy/albums/106123/trex-boner.jpg", "http://no.gd/badger.gif"})
+	bot.Match("boner", func(privmsg Privmsg) {
+		response, err := bot.Sample([]string{"http://files.myopera.com/coxy/albums/106123/trex-boner.jpg", "http://no.gd/badger.gif"})
 		if err != nil {
 			return
 		}
 
 		privmsg.Msg(response)
-	},
+	})
 
-	"badger": func(privmsg Privmsg) {
+	bot.Match("badger", func(privmsg Privmsg) {
 		privmsg.Msg("http://no.gd/badger2.gif")
-	},
+	})
 
-	"dywj": func(privmsg Privmsg) {
+	bot.Match("dywj", func(privmsg Privmsg) {
 		privmsg.Msg("DAMN YOU WILL JESSOP!!!")
-	},
+	})
 
 	// derp, herp
-	"/\\b[dh]erp\\b/": func(privmsg Privmsg) {
+	bot.Match("/\\b[dh]erp\\b/", func(privmsg Privmsg) {
 		privmsg.Msg("http://caius.name/images/qs/herped-a-derp.png")
-	},
+	})
 
-	"/F{2,}U{2,}/": func(privmsg Privmsg) {
+	bot.Match("/F{2,}U{2,}/", func(privmsg Privmsg) {
 		var response string
 
 		if strings.Contains(strings.ToLower(privmsg.Nick), "tomb") {
@@ -95,31 +174,31 @@ var Plugins = map[string]func(privmsg Privmsg){
 		}
 
 		privmsg.Msg(response)
-	},
+	})
 
-	"nextmeat": func(privmsg Privmsg) {
+	bot.Match("nextmeat", func(privmsg Privmsg) {
 		privmsg.Msg("BACNOM")
-	},
+	})
 
-	"/where is (wlll|will)/": func(privmsg Privmsg) {
-		response, err := Sample([]string{"North Tea Power", "home"})
+	bot.Match("/where is (wlll|will)/", func(privmsg Privmsg) {
+		response, err := bot.Sample([]string{"North Tea Power", "home"})
 		if err != nil {
 			return
 		}
 
 		privmsg.Msg(response)
-	},
+	})
 
-	"/^b(oo|ew)bs$/": func(privmsg Privmsg) {
-		response, err := Sample([]string{"(.)(.)", "http://no.gd/boobs.gif"})
+	bot.Match("/^b(oo|ew)bs$/", func(privmsg Privmsg) {
+		response, err := bot.Sample([]string{"(.)(.)", "http://no.gd/boobs.gif"})
 		if err != nil {
 			return
 		}
 
 		privmsg.Msg(response)
-	},
+	})
 
-	"version": func(privmsg Privmsg) {
+	bot.Match("version", func(privmsg Privmsg) {
 		reply := "My current version is"
 
 		if GitCommit != "" {
@@ -133,18 +212,18 @@ var Plugins = map[string]func(privmsg Privmsg){
 		}
 
 		privmsg.Msg(reply)
-	},
+	})
 
 	// Pong plugin
-	"/^(?:\\.|!?\\.?ping)$/": func(privmsg Privmsg) {
+	bot.Match("/^(?:\\.|!?\\.?ping)$/", func(privmsg Privmsg) {
 		privmsg.Msg("pong!")
-	},
+	})
 
-	"/^stats?$/": func(privmsg Privmsg) {
+	bot.Match("/^stats?$/", func(privmsg Privmsg) {
 		privmsg.Msg("http://dev.hentan.caius.name/irc/nwrug.html")
-	},
+	})
 
-	"dance": func(privmsg Privmsg) {
+	bot.Match("dance", func(privmsg Privmsg) {
 		i, err := rand.Int(rand.Reader, big.NewInt(3))
 		if err != nil {
 			i = big.NewInt(1)
@@ -159,11 +238,11 @@ var Plugins = map[string]func(privmsg Privmsg){
 		case 2:
 			privmsg.Msg("http://i.imgur.com/rDDjz.gif")
 		}
-	},
+	})
 
 	// Stabs what he is comanded to. Unless it's himself.
 	// `stab blah` => `* gobot stabs blah`
-	"/stab (.+)/": func(privmsg Privmsg) {
+	bot.Match("/stab (.+)/", func(privmsg Privmsg) {
 		msg := privmsg.Message
 
 		stab_regexp := regexp.MustCompile("stab (.+)")
@@ -176,11 +255,11 @@ var Plugins = map[string]func(privmsg Privmsg){
 
 		// TODO: privmsg.Actionf()
 		privmsg.Action(fmt.Sprintf("/me stabs %s", receiver))
-	},
+	})
 
 	// Listens to channel conversation and inserts title of any link posted, following redirects
 	// `And then I went to www.caius.name` => `gobot: Caius Durling &raquo; Profile`
-	"/.+/": func(privmsg Privmsg) {
+	bot.Match("/.+/", func(privmsg Privmsg) {
 		msg := privmsg.Message
 
 		// Regexp from http://daringfireball.net/2010/07/improved_regex_for_matching_urls - Ta gruber!
@@ -221,8 +300,7 @@ var Plugins = map[string]func(privmsg Privmsg){
 		fmt.Printf("title: %s\n", title)
 
 		privmsg.Msg(title)
-	},
-	//*/
+	})
 
 	// TODO: last
 	// TODO: roll
@@ -242,59 +320,5 @@ var Plugins = map[string]func(privmsg Privmsg){
 	// TODO: twitter user
 	// TODO: commit me
 
-}
-
-func main() {
-	fmt.Printf("Version: %s\nBuilt by: %s\n", GitCommit, BuiltBy) // FU GO
-
-	roomName := "#caius"
-	botName := "gobot"
-
-	con := irc.IRC(botName, botName)
-	err := con.Connect("irc.freenode.net:6667")
-	if err != nil {
-		log.Fatal("Can't connect to freenode")
-	}
-
-	con.AddCallback("001", func(e *irc.Event) {
-		con.Join(roomName)
-	})
-
-	con.AddCallback("PRIVMSG", func(e *irc.Event) {
-		// TODO: have irc.Event contain the room name for the PRIVMSG
-		fmt.Printf("[%6s] %6s: %s\n", roomName, e.Nick, e.Message)
-
-		privmsg := Privmsg{Connection: *con, Event: *e, Message: e.Message, Nick: e.Nick, RoomName: roomName}
-
-		// Plugins!
-		lowerMessage := strings.ToLower(privmsg.Message)
-
-		for matchString := range Plugins {
-			// TODO: check matchString against e.Message
-			if strings.HasPrefix(matchString, "/") && strings.HasPrefix(matchString, "/") {
-				// Yer a regexp Harry
-				regexString := strings.TrimPrefix(matchString, "/")
-				regexString = strings.TrimSuffix(regexString, "/")
-
-				fmt.Printf("regexString: %s\n", regexString)
-
-				// Get on yer bike if we don't match
-				if !regexp.MustCompile(regexString).MatchString(e.Message) {
-					fmt.Printf("Skipping %s plugin - no regexp match\n", matchString)
-					continue
-				}
-			} else {
-				// Just a plain ole string
-				if !strings.Contains(lowerMessage, matchString) {
-					fmt.Printf("Skipping %s plugin - no string match\n", matchString)
-					continue
-				}
-			}
-
-			f := Plugins[matchString]
-			f(privmsg)
-		}
-	})
-
-	con.Loop()
+	bot.Run()
 }
