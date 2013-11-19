8bba83df21c147c54919e61b52f283f1bce77d6c
Caius Durling <dev@caius.name>
2013-11-19 23:10:16 +0000

    Rework gobot's public API somewhat
    * gobot.Bot is now the object created
    * gobot.Gobot() creates a bot for you, complete with initialized plugins map
    * gobot.Bot.Match now expects a regexp.Regexp as the struct key
    * gobot.Bot.MatchString handles simple strings with optional prepended nick

--- gobot.go
+++ gobot.go
@@ -7,22 +7,27 @@ import (
 	"log"
 	"math/big"
 	"regexp"
-	"strings"
 )
 
-type Gobot struct {
+type Bot struct {
 	Name   string // Bot nick
 	Pass   string // Password for nickserv, if required (noop if empty)
 	Room   string // TODO: make an array once PRIVMSG handler can work out source of event
 	Server string // "server.name:port"
 
-	Plugins map[string]func(p Privmsg)
+	Plugins map[*regexp.Regexp]func(p Privmsg)
 
 	Con *irc.Connection
 }
 
+func Gobot() Bot {
+	gobot := Bot{}
+	gobot.Plugins = make(map[*regexp.Regexp]func(p Privmsg))
+	return gobot
+}
+
 // Run the bot. Setup plugins before calling this, blocks execution until program end
-func (bot *Gobot) Run() {
+func (bot *Bot) Run() {
 	bot.Con = irc.IRC(bot.Name, bot.Name) // Use the bot's nick as real name too
 	err := bot.Con.Connect(bot.Server)
 	if err != nil {
@@ -43,31 +48,13 @@ func (bot *Gobot) Run() {
 		privmsg := Privmsg{Connection: *bot.Con, Event: *e, Message: e.Message, Nick: e.Nick, RoomName: bot.Room}
 
 		// Plugins!
-		lowerMessage := strings.ToLower(privmsg.Message)
-
-		for matchString := range bot.Plugins {
-			// TODO: check matchString against e.Message
-			if strings.HasPrefix(matchString, "/") && strings.HasPrefix(matchString, "/") {
-				// Yer a regexp Harry
-				regexString := strings.TrimPrefix(matchString, "/")
-				regexString = strings.TrimSuffix(regexString, "/")
-
-				fmt.Printf("regexString: %s\n", regexString)
-
-				// Get on yer bike if we don't match
-				if !regexp.MustCompile(regexString).MatchString(e.Message) {
-					fmt.Printf("Skipping %s plugin - no regexp match\n", matchString)
-					continue
-				}
-			} else {
-				// Just a plain ole string
-				if !strings.Contains(lowerMessage, matchString) {
-					fmt.Printf("Skipping %s plugin - no string match\n", matchString)
-					continue
-				}
+		for matcher := range bot.Plugins {
+			if !matcher.MatchString(e.Message) {
+				fmt.Printf("Skipping %s plugin\n", matcher)
+				continue
 			}
 
-			f := bot.Plugins[matchString]
+			f := bot.Plugins[matcher]
 			f(privmsg)
 		}
 	})
@@ -75,13 +62,31 @@ func (bot *Gobot) Run() {
 	bot.Con.Loop()
 }
 
-// Add handler for trigger
-func (bot *Gobot) Match(trigger string, handler func(privmsg Privmsg)) {
+func (bot *Bot) NickRegexp(input string) *regexp.Regexp {
+	// TODO: case insensitive
+	input = fmt.Sprintf("(?:\\A|%s:? )%s\\z", bot.Name, input)
+	return regexp.MustCompile(input)
+}
+
+// Add handler for string trigger, using nick regexp to wrap the string
+func (bot *Bot) MatchString(trigger string, handler func(privmsg Privmsg)) {
+	// Match full phrase, optionally prepended with <nick>: or <nick>
+	// eg:
+	//    `help'
+	//    `mybot: help'
+	//    `mybot help'
+	matcher := bot.NickRegexp(trigger)
+	bot.Match(matcher, handler)
+}
+
+// Add handler for regexp
+// regexp is untouched
+func (bot *Bot) Match(trigger *regexp.Regexp, handler func(privmsg Privmsg)) {
 	bot.Plugins[trigger] = handler
 }
 
 // Helper method for returning "random" responses
-func (bot *Gobot) Sample(arr []string) (string, error) {
+func (bot *Bot) Sample(arr []string) (string, error) {
 	max := int64(len(arr))
 	i, err := rand.Int(rand.Reader, big.NewInt(max))
 	if err != nil {
